769d869f052844e3e102ac1da32cec0d47d9a73f
BeikerJ <121730737+BeikerJ@users.noreply.github.com>
2025-10-22 22:03:46 -0500

    Archivos backend

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "index")));
+
+app.listen(PORT, () => console.log(`🚀 Servidor ejecutándose en http://localhost:${PORT}`));
